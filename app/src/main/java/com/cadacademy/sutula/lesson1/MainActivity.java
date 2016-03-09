8a7f5b9925f5f22e8b3ced35d520965cf86e24d6
sanchousese <saniasutula@gmail.com>
2016-03-09 22:08:25 +0200

    colors, button names added and views bind with xml

--- app/src/main/java/com/cadacademy/sutula/lesson1/MainActivity.java
+++ app/src/main/java/com/cadacademy/sutula/lesson1/MainActivity.java
@@ -2,12 +2,27 @@ package com.cadacademy.sutula.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rlBackground;
+
+    Button btnRed;
+    Button btnYellow;
+    Button btnGreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rlBackground = (RelativeLayout) findViewById(R.id.rlBackground);
+        btnRed = (Button) findViewById(R.id.btnRed);
+        btnYellow = (Button) findViewById(R.id.btnYellow);
+        btnGreen = (Button) findViewById(R.id.btnGreen);
+
+
     }
 }
